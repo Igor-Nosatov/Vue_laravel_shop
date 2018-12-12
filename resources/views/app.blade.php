ec5c1d0801c3113567f871b79b6f731e19f07a76
Igor-Nosatov <sinevor13@gmail.com>
2018-12-12 20:56:23 +0200

    concta js and css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,23 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-    <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
-    <link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
-    <link href="plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
-    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
-    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
-    <link rel="stylesheet" type="text/css" href="plugins/slick-1.8.0/slick.css">
-    <link rel="stylesheet" type="text/css" href="styles/main_styles.css">
-    <link rel="stylesheet" type="text/css" href="styles/responsive.css">
+    <link href="{{ mix('css/all.css') }}" type="text/css" rel="stylesheet" />
     <meta name="csrf-token" value="{{ csrf_token() }}" />
 </head>
 
 <body>
     <div id="app">
     </div>
-    <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
-
+    <script src="{{ mix('js/all.js') }}" type="text/javascript"></script>
 </body>
-
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,27 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.styles([
+    'resources/adds/styles/bootstrap4/bootstrap.min.css',
+    'resources/adds/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css',
+    'resources/adds/plugins/OwlCarousel2-2.2.1/owl.carousel.css',
+    'resources/adds/plugins/OwlCarousel2-2.2.1/owl.theme.default.css',
+    'resources/adds/plugins/OwlCarousel2-2.2.1/animate.css',
+    'resources/adds/plugins/slick-1.8.0/slick.css',
+    'resources/adds/styles/main_styles.css',
+    'resources/adds/styles/responsive.css'
+], 'public/css/all.css');
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.scripts([
+       'resources/adds/js/jquery-3.3.1.min.js',
+       'resources/adds/styles/bootstrap4/popper.js',
+       'resources/adds/styles/bootstrap4/bootstrap.min.js',
+       'resources/adds/plugins/greensock/TweenMax.min.js',
+       'resources/adds/plugins/greensock/TimelineMax.min.js',
+       'resources/adds/plugins/scrollmagic/ScrollMagic.min.js',
+       'resources/adds/plugins/greensock/animation.gsap.min.js',
+       'resources/adds/plugins/greensock/ScrollToPlugin.min.js',
+       'resources/adds/plugins/OwlCarousel2-2.2.1/owl.carousel.js',
+       'resources/adds/plugins/slick-1.8.0/slick.js',
+       'resources/adds/plugins/easing/easing.js',
+       'resources/adds/js/custom.js'
+   ], 'public/js/all.js');
